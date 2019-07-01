5c32a88b65a2d9610987a715ef94a0b29780729b
machiren <machiren111@gmail.com>
2019-07-01 16:17:19 +0900

    add-employee_nuber_create

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,13 +61,13 @@
                             </div>
                         </div>
 
-                        <!-- <div class="form-group row">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('社員ナンバー') }}</label>
+                        <div class="form-group row">
+                            <label for="employee_number" class="col-md-4 col-form-label text-md-right">{{ __('社員ナンバー') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password-confirm" type="text" class="form-control" name="password_confirmation" required autocomplete="new-password">
+                                <input id="employee_number" type="text" class="form-control" name="employee_number" required autocomplete="employee_number">
                             </div>
-                        </div> -->
+                        </div>
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
